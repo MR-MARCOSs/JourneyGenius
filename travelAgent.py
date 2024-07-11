db4f9e8eeb3cc53198dbe1e21f2605924fc75bf9
MR-MARCOSs <marcosdudu001@gmail.com>
2024-07-10 23:48:59 -0300

    First commit

---
+++ travelAgent.py
@@ -0,0 +1,33 @@
+import os
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_community.agent_toolkits.load_tools import load_tools
+from langchain.agents import create_react_agent, AgentExecutor
+from langchain import hub
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import Chroma
+import bs4
+
+llm = ChatOpenAI(model="gpt-3.5-turbo")
+query = """
+    vou viajar para Palmas Tocantins em Agosto de 2024
+    Quero que faça um roteiro de viagens para mim com eventos que irão ocorrer na data da viagem e com o preço da passagem
+"""
+def researchAgent(query,llm): 
+
+    tools = load_tools(["ddg-search", "wikipedia"], llm=llm)
+
+
+    prompt = hub.pull("hwchase17/react")
+    agent = create_react_agent(
+        llm,
+        tools,
+        prompt
+        
+    )
+
+    agent_executor = AgentExecutor(agent=agent, tools=tools, prompt=prompt, verbose=True )
+    webContext = agent_executor.invoke({"input": query})
+    return webContext['output']
+
+
+
