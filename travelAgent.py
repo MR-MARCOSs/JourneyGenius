ab88fd0b71d11f889227e4a7e065e18343f6f109
MR-MARCOSs <marcosdudu001@gmail.com>
2024-07-13 23:52:33 -0300

    added to aws

--- travelAgent.py
+++ travelAgent.py
@@ -10,56 +10,38 @@ from langchain_text_splitters.character import RecursiveCharacterTextSplitter
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableSequence
 
+OPENAI_API_KEY= os.environ['OPENAI_API_KEY']
 
 llm = ChatOpenAI(model="gpt-3.5-turbo")
-query = """
-    vou viajar para Londres em Agosto de 2024
-    Quero que faça um roteiro de viagens para mim com eventos que irão ocorrer na data da viagem e com o preço da passagem
-"""
+
 def researchAgent(query,llm): 
-
-
     tools = load_tools(["ddg-search", "wikipedia"], llm=llm)
-
-
     prompt = hub.pull("hwchase17/react")
     agent = create_react_agent(
         llm,
         tools,
         prompt
-        
     )
-    
-
     agent_executor = AgentExecutor(agent=agent, tools=tools, prompt=prompt, verbose=True )
     webContext = agent_executor.invoke({"input": query})
     return webContext['output']
-
 
 def loadData():
     loader = WebBaseLoader(
     web_paths=("https://www.dicasdeviagem.com/inglaterra/",),
     bs_kwargs=dict(parse_only=bs4.SoupStrainer(class_=("postcontentwrap", "pagetitleloading background-imaged loading-dark"))))
-    print("3")
     docs = loader.load()
-    print("4")
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    print("5")
     splits = text_splitter.split_documents(docs)
-    print("6")
-    vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
-    print("7")
+    save_folder = "/tmp/chroma"
+    os.makedirs(save_folder, exist_ok=True)
+    vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings(), persist_directory=save_folder)
     retriever = vectorstore.as_retriever()
-    print("8")
     return retriever
 
 def getRelevantDocs(query):
-    print("1")
     retriever = loadData()
-    print("2")
     relevant_documents = retriever.invoke(query)
-    print("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _")
-    print("\n\n\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _" + relevant_documents + "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\n\n\n")
     return relevant_documents
 
 def supervisorAgent(query, llm, webContext, relevant_documents):
@@ -71,16 +53,13 @@ def supervisorAgent(query, llm, webContext, relevant_documents):
     Usuário: {query}
     Assistente: 
     """
-
     prompt = PromptTemplate(
         input_variables= ['webContext', 'relevant_documents', 'query'],
         template = prompt_template
     )
-
     sequence = RunnableSequence(
         prompt | llm
     )
-
     response = sequence.invoke({"webContext": webContext, "relevant_documents": relevant_documents, "query": query})
     return response
 
@@ -90,8 +69,7 @@ def getResponse(query, llm):
     response = supervisorAgent(query, llm, webContext, relevant_documents)
     return response
 
-
-
-print("\n\n\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _" +getResponse(query, llm).content + "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\n\n\n")
-print("finish")
-
+def lambda_handler(event, context):
+    query = event.get("question")
+    response = getResponse(query,llm).content
+    return {"body": response, "status": 200}
