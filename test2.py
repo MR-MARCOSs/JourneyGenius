a0f446f430a00f7b24dbe26895cc6e47341f9f86
MR-MARCOSs <marcosdudu001@gmail.com>
2024-07-23 23:44:08 -0300

    new test2.py

--- test2.py
+++ test2.py
@@ -3,15 +3,61 @@ from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_community.agent_toolkits.load_tools import load_tools
 from langchain.agents import create_react_agent, AgentExecutor
 from langchain import hub
-from langchain_community.document_loaders import *
+from langchain_community.document_loaders import WebBaseLoader
 from langchain_community.vectorstores import Chroma
 import bs4
-from langchain_text_splitters.character import CharacterTextSplitter
+import sqlite3
+from langchain_text_splitters.character import RecursiveCharacterTextSplitter
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableSequence
 
 
 llm = ChatOpenAI(model="gpt-3.5-turbo")
+
+
+def init_db():
+    conn = sqlite3.connect('chat_history.db')
+    c = conn.cursor()
+    c.execute('''
+    CREATE TABLE IF NOT EXISTS human_message
+              id VARCHAR(16) PRIMARY KEY,
+              message_number INT AUTOINCREMENT,
+              humam_message TEXT,
+              
+    ''')
+    c.execute('''
+    CREATE TABLE IF NOT EXISTS response_from_ai
+              id_message VARCHAR(16) NOT NULL,
+              response_from_ai TEXT,
+              FOREIGN KEY (id_mensage) REFERENCES human_message (id)
+    ''')
+    conn.commit()
+    conn.close()
+
+def add_message(id, human_message, response_from_ai):
+    conn = sqlite3.connect('chat_history.db')
+    c = conn.cursor()
+    c.execute(
+        '''
+    INSERT INTO human_message (id, human_message)
+    VALUES (:id, :human_message)
+    ''', {
+        'id': id,
+        'human_message': human_message
+    }
+    ),
+    c.execute(
+        '''
+    INSERT INTO response_from_ai (id, response_from_ai)
+    VALUES (:id, :response_from_ai)
+    ''', {
+        'id': id,
+        'response_from_ai': response_from_ai
+    })
+
+
+
+
 
 def researchAgent(query,llm): 
 
@@ -24,49 +70,49 @@ def researchAgent(query,llm):
         llm,
         tools,
         prompt
+
     )
+
 
     agent_executor = AgentExecutor(agent=agent, tools=tools, prompt=prompt, verbose=True, handle_parsing_errors=True)
     webContext = agent_executor.invoke({"input": query})
     return webContext['output']
 
-
-def loadData(chat_history):
-    loader= 
-    documents = loader.load()
+'''
+def loadData():
+    loader = WebBaseLoader(
+    web_paths=("https://www.dicasdeviagem.com/inglaterra/",),
+    bs_kwargs=dict(parse_only=bs4.SoupStrainer(class_=("postcontentwrap", "pagetitleloading background-imaged loading-dark"))))
     print("3")
-    docs = chat_history
+    docs = loader.load()
     print("4")
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    docs = text_splitter.split_documents(documents)
-
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     print("5")
-
+    splits = text_splitter.split_documents(docs)
     print("6")
-    vectorstore = Chroma.from_documents(documents=docs, embedding=OpenAIEmbeddings())
+    vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
     print("7")
     retriever = vectorstore.as_retriever()
     print("8")
     return retriever
 
-def getRelevantDocs(query, chat_history):
+def getRelevantDocs(query):
     print("1")
-    retriever = loadData(chat_history)
+    retriever = loadData()
     print("2")
     relevant_documents = retriever.invoke(query)
-    print("\n\n\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _docs")
+    print("\n\n\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _")
     print(relevant_documents)
-    print("\n\n\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _docs")
     return relevant_documents
-
-def supervisorAgent(query, llm, webContext, relevant_docs):
+'''
+def supervisorAgent(query, llm, webContext, chat_history):
     prompt_template= """
     Você é o agente de viagens JourneyGenius. Sua resposta final deverá ser um roteiro de viagem completo e detalhado ou a continuação da conversa com o usuário.
     Utilize o contexto de eventos(se for útil), preços de passagens, input do usuário para elaborar o roteiro e o histórico de conversa até o momento.
 
     Contexto: {webContext}
     Usuário: {query}
-    Histórico: {relevant_docs}
+    Histórico: {chat_history}
     Assistente: 
     """
 
@@ -79,7 +125,7 @@ def supervisorAgent(query, llm, webContext, relevant_docs):
         prompt | llm
     )
 
-    response = sequence.invoke({"webContext": webContext, "query": query, "relevant_docs": relevant_docs})
+    response = sequence.invoke({"webContext": webContext, "query": query, "chat_history": chat_history})
     return response
 
 def getResponse(llm):
@@ -109,12 +155,12 @@ def getResponse(llm):
         print("dps")
         print(total_characters)
         print(len(chat_history["Humano"]))            
-        print("\n\n\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\n\n\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _")
+        print("\n\n\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ \n\n\n _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _")
         print(chat_history)
         query = input("Diga: ")
         webContext = researchAgent(query, llm)
-        relevant_docs=getRelevantDocs(query,chat_history)
-        response = supervisorAgent(query, llm, webContext, relevant_docs)
+
+        response = supervisorAgent(query, llm, webContext, chat_history)
         chat_history["Humano"].append(f"{msg_count}ª mensagem:" + query)
         chat_history["Você"].append(f"Tua resposta a {msg_count}ª mensagem:"+response.content)
         msg_count+=1
